437a480a2d500cd603b61a3afa1b284f1dbcdab2
RodolphoGLC <rodolpho.geouerj20@gmail.com>
2022-09-28 11:18:02 -0300

    Inclusão de mais exemplos e um projeto de Calculadora

--- Program.cs
+++ Program.cs
@@ -2,60 +2,19 @@
 
 // ------------------------------------------------------------------------------------------------------------
 
-//Amanhã começa aqui
+//Parte do código para a Calculadora
 
-// ------------------------------------------------------------------------------------------------------------
+//Calculadora calc = new Calculadora();
 
-//Valor maximo e minimo de uma variavel numérica 
-
-//int a = int.MaxValue;
-//int a = int.MinValue;
-
-// Console.WriteLine(inteiro);
-
-// ------------------------------------------------------------------------------------------------------------
-
-//Converter qualquer uma para string, pq a string está presente em todas as variaveis
-
-// int inteiro = 5;
-// string a = inteiro.ToString();
-
-// Console.WriteLine(inteiro);
-
-// ------------------------------------------------------------------------------------------------------------
-
-//Conversão de string para inteiro 2 jeitos 
-
-//int a = Convert.ToInt32("5"); //para uma especifica, aplicavel para valores nulos (null) = Use esse  
-//int a = Parse("5"); //para a que se pede antes, valores nulos (null) darão erros
-
-//Console.WriteLine(a);
-
-// ------------------------------------------------------------------------------------------------------------
-
-//Exemplo de soma 
-// int a = 25;
-// int b = 40;
-// int c = a + b;
-
-// c = c + 5; //Vai alterar o valor final de c, o novo será (a+b)+5. Antigo valor de c + 5
-// c += 5; faz a mesma função do acima
-// Console.WriteLine(c);
-
-// ------------------------------------------------------------------------------------------------------------
-
-//Aprendendo sobre variaveis e classes - Ctrl + k + c + parte do codigo selecionada = Comentar tudo
-// string apresentação = "Olá seja bem vindo.";
-// int quantidade = 1;
-// double valor = 1.80;
-// decimal preço = 1.80M;
-// bool condicao = true;
-
-// Console.WriteLine(apresentação);
-// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
-// Console.WriteLine("Valor da variavel valor: " + valor);
-// Console.WriteLine("Valor da variavel preço: " + preço);
-// Console.WriteLine("Valor da variavel condicao: " + condicao);
+// calc.Somar(10, 10);
+// calc.Subtrair(40, 20);
+// calc.Multiplicar(4, 5);
+// calc.Dividir(100, 5);
+// calc.Potencia(3, 3);
+// calc.Seno(45);
+// calc.Cosseno(45);
+// calc.Tangente(45);
+//calc.RaizQuadrada (9);
 
 // ------------------------------------------------------------------------------------------------------------
 
