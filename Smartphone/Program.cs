d3390f6fdd1da593de6b1b7134a1353f18eead83
Rodolpho Coutinho <91194767+RodolphoGLC@users.noreply.github.com>
2022-10-17 16:40:49 -0300

    Desafio Smartphone Concluído!!!!

---
+++ Smartphone/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone da Nokia");
+Smartphone nokia = new Nokia("1234", "Modelo Nokia", "000000000", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp"); //Fazer isso pelo polimorfismo
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone da Apple");
+Smartphone iphone = new Iphone("5678", "Modelo Apple", "333333333", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook"); //Fazer isso pelo polimorfismo
